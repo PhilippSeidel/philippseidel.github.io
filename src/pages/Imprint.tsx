6c9c17c5852ed39a789d0bc3f7b0813c5fb9a4a1
Philipp Seidel <spam@philipp-seidel.de>
2025-06-18 16:53:15 +0100

    added legal disclaimer to imprint

--- src/pages/Imprint.tsx
+++ src/pages/Imprint.tsx
@@ -4,12 +4,17 @@ const Imprint: React.FC = () => {
   return (
     <section>
       <h1><i className="bi bi-fingerprint"></i> Imprint</h1>
-        <p className="lead">Philipp Seidel<br/>
+        <p className="lead" style={{padding: "50px"}}>Philipp Seidel<br/>
             Yorckstraße 11<br/>
             79110 Freiburg<br/>
             Germany<br/><br/>
             If you wish to contact me, please do so via <a href="mailto:mail@philipp-seidel.de">E-Mail</a>
         </p>
+        <figure className='text-center'>
+          <figcaption className="blockquote-footer">
+          Mit Urteil vom 12. Mai 1998 - 312 O 85/98 - 'Haftung für Links' hat das Landgericht (LG) Hamburg entschieden, dass man durch das Setzen eines Links, die Inhalte der gelinkten Seite ggf. mit zu verantworten hat. Dies kann - so das LG - nur dadurch verhindert werden, dass man sich ausdrücklich von diesen Inhalten distanziert. Hiermit distanziere ich mich ausdrücklich von den verlinkten Seiten.
+          </figcaption>
+        </figure>
     </section>
   );
 };
